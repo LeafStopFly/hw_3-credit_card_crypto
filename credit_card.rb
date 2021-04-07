0279b1eb5e6d225db56e91a817a494ede2983e58
亭寶寶 <havestop@havestopchai.local>
2021-04-08 03:06:52 +0800

    Hashing

--- credit_card.rb
+++ credit_card.rb
@@ -2,6 +2,8 @@
 
 require_relative './luhn_validator'
 require 'json'
+require 'base64'
+require 'rbnacl'
 
 ## CreditCard
 class CreditCard
@@ -37,6 +39,7 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    Base64.decode64(card_s)
   end
 
   # return a hash of the serialized credit card object
@@ -45,6 +48,7 @@ class CreditCard
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    Base64.encode64(to_s).hash
   end
 
   # return a cryptographically secure hash
@@ -52,5 +56,6 @@ class CreditCard
     # TODO: implement this method
     #   - Use sha256 from openssl to create a cryptographically secure hash.
     #   - Credit cards with identical information should produce the same hash
+    RbNaCl::Hash.sha256(Base64.encode64(to_s))
   end
 end
